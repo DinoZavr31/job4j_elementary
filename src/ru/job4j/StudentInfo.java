fefd34f06852e7788787c3c52eaf9846be00b751
rinat <rinatlatypov94@gmail.com>
2020-10-13 10:09:03 +0300

    3.2. Фиксация изменений на github [#386865]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] agrs) {
+        System.out.println("Rinat Latypov");
+        System.out.println("31.10.1994");
+    }
+}
